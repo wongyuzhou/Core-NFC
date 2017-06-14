be1387f1ac95e540cbf6c6d3651809e764b7c76f
wyz <smallyuzhou@github.com>
2017-06-14 09:59:13 +0800

    Initial Commit

---
+++ autobole_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  autobole_test
+//
+//  Created by wyz on 2017/6/14.
+//  Copyright © 2017年 wyz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
